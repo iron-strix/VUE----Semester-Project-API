9eb5e2dbfdf29d30d3a73eec59cc2131f015ade4
iron-strix <88483614+iron-strix@users.noreply.github.com>
2022-12-01 19:44:26 -0600

    added utilities, security, and basic routing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,33 @@
+import './utils/dotenv'
+
 import express from 'express'
+import morgan from 'morgan'
+import cors from 'cors'
+import helmet from 'helmet'
+import bodyParser from 'body-parser'
+import logger from './utils/logger'
+import router from './routes'
+import { notFound, errorHandler } from './utils/errors'
+
+const port = parseInt(process.env.PORT, 10) || 3000
 
 const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.use(morgan(process.env.MORGAN_LOG)) // logging
+app.use(
+  cors({
+    origin: process.env.ORIGIN,
+    exposedHeaders: [],
+  }),
+) // cross origin
+app.use(helmet()) // secures headers
+app.use(bodyParser.json()) // allows parsing body request
 
-app.listen(port)
+app.use(router)
+
+app.use(notFound)
+app.use(errorHandler)
+
+app.listen(port, () =>
+  logger.info(`Application started at http://localhost:${process.env.PORT}`),
+)
