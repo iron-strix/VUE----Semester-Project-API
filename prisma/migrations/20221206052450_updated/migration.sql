ea357bc47b86aec3aa83a86327495850ac2ef04b
iron-strix <88483614+iron-strix@users.noreply.github.com>
2022-12-05 23:25:53 -0600

    updated user schema

---
+++ prisma/migrations/20221206052450_updated/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the column `password` on the `User` table. All the data in the column will be lost.
+
+*/
+-- DropIndex
+DROP INDEX "User_name_key";
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "name",
+DROP COLUMN "password";
